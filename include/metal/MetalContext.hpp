89a514a6920f90c701837757fa99b41de689fe78
SteveCutler <stephen.c.cutler@gmail.com>
2026-08-02 15:50:54 -0700

    added metal headers, beginning to build out the metal compute framework. slow going.

---
+++ include/metal/MetalContext.hpp
@@ -0,0 +1,28 @@
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+
+class MetalContext {
+
+    public:
+        //constructor
+        MetalContext(std::string library){}
+        
+        //Create kernel pipeline
+        MTL::ComputePipelineState* CreatePipelineState(std::string kernel_name, NS::Error* error){}
+        
+        //destructor
+        ~MetalContext(){}
+
+        //getters
+        MTL::Device* get_device(){}
+        MTL::CommandQueue* get_commandqueue(){}
+        
+        std::string m_library_name;
+
+    private:
+        MTL::Device* m_device;
+        MTL::CommandQueue* m_commandqueue;
+        MTL::Library* m_library;
+
+
+};
---
+++ src/metal/MetalContext.cpp
@@ -0,0 +1,132 @@
+#include "MetalContext.hpp"
+#include <iostream>
+
+MetalContext::MetalContext(std::string library):
+m_library_name(library){
+
+    //Intialize metal device
+    
+    NS::AutoreleasePool* autoreleasePool =
+        NS::AutoreleasePool::alloc()->init();
+
+    m_device = MTL::CreateSystemDefaultDevice();
+
+
+    if (m_device == nullptr) {
+        std::cerr << "Error: no Metal device found.\n";
+        autoreleasePool->release();
+        return;
+    }
+
+     std::cout << "Metal device: "
+              << m_device->name()->utf8String()
+              << '\n';
+
+
+    //Create command queue
+
+    m_commandqueue =
+        m_device->newCommandQueue();
+
+    if (m_commandqueue == nullptr) {
+        std::cerr << "Error creating command queue.\n";
+        autoreleasePool->release();
+        return;
+    }
+    
+    //Load metal library
+
+    NS::Error* error = nullptr;
+
+    NS::String* libraryPath =
+        NS::String::string(
+            m_library_name.c_str(),
+            NS::UTF8StringEncoding
+        );
+
+    NS::URL* libraryURL =
+        NS::URL::fileURLWithPath(libraryPath);
+
+    MTL::Library* m_library =
+        m_device->newLibrary(libraryURL, &error);
+
+    if (m_library == nullptr) {
+        std::cerr << "Error loading FluidKernels.metallib";
+
+        if (error != nullptr) {
+            std::cerr << ": "
+                      << error->localizedDescription()->utf8String();
+        }
+
+        std::cerr << '\n';
+
+        m_device->release();
+        autoreleasePool->release();
+        return;
+    }
+    
+}
+
+MTL::ComputePipelineState* MetalContext::CreatePipelineState(std::string kernel_name, NS::Error* error){
+
+    MTL::Function* function = m_library->newFunction(
+        NS::String::string(
+            kernel_name.c_str(),
+            NS::UTF8StringEncoding
+        )
+    );
+
+    if(function == nullptr){
+        std::cerr << "Error: kernel " << kernel_name << " was not found. \n";
+        
+        m_library->release();
+        m_device->release();
+        return;
+    }
+
+    MTL::ComputePipelineState* pipeline = 
+        m_device->newComputePipelineState(function, &error);
+    
+        if(pipeline == nullptr){
+            std::cerr << "Error creating compute pipeline";
+
+            if(error != nullptr){
+                std::cerr << ": " << error->localizedDescription()->utf8String() << "\n";
+            }
+
+            function->release();
+            m_library->release();
+            m_device->release();
+            return;
+        }
+
+    function->release();
+    return pipeline;
+}
+
+MetalContext::~MetalContext(){
+
+    if(m_library != nullptr){
+        m_library->release();
+    }
+    if(m_commandqueue != nullptr){
+        m_commandqueue->release();
+    }
+    if(m_device != nullptr){
+        m_device->release();
+    }
+
+    std::cout << "Metal Context destroyed" << std::endl;
+}
+
+//getters
+MTL::Device* MetalContext::get_device(){
+    return m_device;
+}
+MTL::CommandQueue* MetalContext::get_commandqueue(){
+    return m_commandqueue;
+}
+
+
+//MTL::CommandQueue* m_commandqueue = MTL::Create
+//MTL::Library* m_library = MTL::Lib
